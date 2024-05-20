1cdaf570a2b1887d8a0d9a1754207b8bf4edcbec
TristanLong <trieukimlong102000@gmail.com>
2024-05-20 17:49:31 +0700

    By TristanLong

---
+++ connectComponent/SourceList.cpp
@@ -0,0 +1,99 @@
+﻿#include "iostream"
+#include "fstream"
+using namespace std;
+const int maxDinh = 20;
+bool visited[maxDinh] = { false };
+
+struct node {
+	int dinhKe;
+	int trongSo;
+	node* link;
+};
+
+struct danhSachKe {
+	node* dsk[maxDinh];
+	int soDinh; // số đỉnh của graph
+};
+
+void readFile(danhSachKe& list) {
+	ifstream inFile("AdjacencyList.txt");
+	if (!inFile.is_open()) {
+		cout << "\nKhong the mo tep.";
+		return;
+	}
+	// cho danh sách = null hết
+	for (int i = 0; i < maxDinh; i++)
+		list.dsk[i] = NULL;
+	// Nhap số đỉnh tổng
+	inFile >> list.soDinh;
+	for (int i = 0; i < list.soDinh; i++) {
+		int soDinhKe;
+		inFile >> soDinhKe;
+		for (int j = 0; j < soDinhKe; j++) {
+			node* newNode = new node();
+			inFile >> newNode->dinhKe;
+			inFile >> newNode->trongSo;
+			newNode->link = NULL;
+			// Thêm vào single list theo đỉnh tương ứng 
+			if (list.dsk[i] == NULL)  // chưa có đỉnh kề nào hết 
+				list.dsk[i] = newNode;
+			else { // đã có đỉnh kề trước đó thêm vào cuối single list 
+				node* temp = list.dsk[i];
+				while (temp->link != NULL)
+					temp = temp->link;
+				temp->link = newNode;
+			}
+		}
+	}
+}
+
+void xuatDanhSachKe(danhSachKe list) {
+	cout << "GRAPH";
+	for (int i = 0; i < list.soDinh; i++) {
+		cout << "\n(" << i << "): ";
+		node* pointer = list.dsk[i];
+		while (pointer) {
+			cout << "(" << pointer->dinhKe << "," << pointer->trongSo << ") ";
+			pointer = pointer->link;
+		}
+	}
+}
+
+void DFS(int u, danhSachKe list) {
+	cout << u << " ";
+	visited[u] = true; // đánh dấu đỉnh u đã được thăm 
+
+	// Duyệt qua tất cả các đỉnh kề của u
+	node* temp = list.dsk[u];
+	while (temp != NULL) {
+		int v = temp->dinhKe;
+		if (!visited[v])
+			DFS(v, list);
+		temp = temp->link;
+	}
+}
+
+void connectComponent(danhSachKe list) {
+	int count = 0;
+	for (int i = 1; i <= list.soDinh; i++) {
+		if (visited[i] == false) {
+			count++;
+			cout << "\nCac dinh thuoc thanh phan lien thong thu " << count << ": ";
+			DFS(i, list);
+			cout << endl;
+		}
+	}
+	if (count == 1)
+		cout << "Do thi lien thong";
+	else {
+		cout << "Do thi khong lien thong";
+		cout << "\nSo thanh phan lien thong cua do thi: " << count;
+	}
+}
+
+int main() {
+	danhSachKe list;
+	readFile(list);
+	xuatDanhSachKe(list);
+	connectComponent(list);
+}
---
+++ connectComponent/SourceMatrix.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include <queue>
+using namespace std;
+
+const int MAX = 20;
+
+struct maTrix {
+    int maTranKe[MAX][MAX];
+    int soDinh;
+};
+
+void readFile(maTrix& mtk) {
+    ifstream inFile("AdjacencyMatrix.txt");
+    if (!inFile.is_open()) {
+        cout << "\nKhong the mo tep";
+        return;
+    }
+
+    inFile >> mtk.soDinh;
+    for (int i = 0; i < mtk.soDinh; i++) {
+        for (int j = 0; j < mtk.soDinh; j++) {
+            inFile >> mtk.maTranKe[i][j];
+        }
+    }
+    inFile.close();
+}
+
+void outputMaTrix(maTrix mtk) {
+    cout << "\nMA TRAN KE: " << endl;
+    for (int i = 0; i < mtk.soDinh; i++) {
+        for (int j = 0; j < mtk.soDinh; j++) {
+            cout << mtk.maTranKe[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+bool visited[MAX] = { false };
+
+void DFS(int u, maTrix graph) {
+    cout << u << " ";
+    visited[u] = true;
+
+    for (int i = 0; i < graph.soDinh; ++i) {
+        if (graph.maTranKe[u][i] != 0 && !visited[i]) {
+            DFS(i, graph);
+        }
+    }
+}
+
+
+void connectComponent(maTrix graph) {
+    int count = 0;
+    for (int i = 0; i < graph.soDinh; i++) {
+        if (!visited[i]) {
+            count++;
+            cout << "\nCac dinh thuoc thanh phan lien thong thu " << count << ": ";
+            DFS(i, graph);
+            cout << endl;
+        }
+    }
+    if (count == 1)
+        cout << "\nDo thi lien thong";
+    else {
+        cout << "\nDo thi khong lien thong";
+        cout << "\nSo thanh phan lien thong cua do thi: " << count;
+    }
+}
+
+int main() {
+    maTrix graph;
+    readFile(graph);
+    outputMaTrix(graph);
+    connectComponent(graph);
+}
